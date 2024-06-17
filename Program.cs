45340e3353931400376e24950660887228662718
Gabriel Freitas <gabriel.freitas@bit.com.br>
2024-06-17 13:31:31 -0300

    Modified to Media de Notas

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿namespace phellowrld;
+﻿using System.Net.Http.Headers;
+
+namespace phellowrld;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int nota1 = 9;
+        int nota2 = 8;
+        int media = (nota1 + nota2)/2;
+        if (media > 7)
+            Console.WriteLine("Aprovado!");
+        else if (media < 7)
+            Console.WriteLine("Reprovado!");
     }
 }
--- obj/Debug/net8.0/phellowrld.AssemblyInfo.cs
+++ obj/Debug/net8.0/phellowrld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("phellowrld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6fd2d7f682d3c80ea5993112e25af46c9ee5827")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39781288138a2953ad760550a74475245caa53ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("phellowrld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("phellowrld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
